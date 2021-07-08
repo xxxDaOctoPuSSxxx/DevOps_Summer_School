15293eeb83a749a48dbcc6788c96a386e27fde52
Roman Hryshchenko <bookoowest@gmail.com>
2021-07-09 02:11:55 +0300

    v1.1

--- Task_2_autobackup/autobackup.sh
+++ Task_2_autobackup/autobackup.sh
@@ -17,7 +17,7 @@ filename=$file_name_prefix$date_of_backup".sql"
 #check if service running
 if [[ ${#mariadbservice} == "0" ]] # function retun 0 --> show error
 then
-echo $(tput setaf 1)"Data base service not started!!! Run it to use sudo  syste>
+echo $(tput setaf 1)"Data base service not started!!! Run it to use sudo  systemctl start mariadb>
 exit 1
 else
 mysqldumpresul=`mysqldump -u $USERNAME -p$PASSWORD moodle > $filename`
